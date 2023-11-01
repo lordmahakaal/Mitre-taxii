7e6e2a2265b876dfd2fb85b93ac3661c684f50aa
lordmahakaal <149451328+lordmahakaal@users.noreply.github.com>
2023-11-01 12:51:19 +0530

    Update taxii.py

--- taxii.py
+++ taxii.py
@@ -1,62 +1,87 @@
 import os
-import logging
 import pandas as pd
 import matplotlib.pyplot as plt
 import openai
 from stix2 import FileSystemSource, FileSystemSink, TAXIICollectionSource, Filter, Indicator, parse
 from taxii2client import Server, Collection
 import git
+import logging
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Load sensitive data from environment variables
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TAXII_COLLECTION_URL = os.getenv("TAXII_COLLECTION_URL")
-NETWORK_LOG_FILE = os.getenv("NETWORK_LOG_FILE")
-MITRE_REPOSITORY_URL = os.getenv("MITRE_REPOSITORY_URL")
-MITRE_CLONE_DIR = os.getenv("MITRE_CLONE_DIR")
+logging.basicConfig(filename='app.log', level=logging.ERROR)
+logger = logging.getLogger(__name)
 
 # Initialize a FileSystemSink to store the STIX/TAXII data locally
 STIX_SINK_DIRECTORY = "/path/to/log_data"
-sink = FileSystemSink(STIX_SINK_DIRECTORY)
+
+def initialize_stix_sink(directory):
+    try:
+        sink = FileSystemSink(directory)
+        return sink
+    except Exception as e:
+        logging.error("Failed to initialize STIX sink: %s", str(e)
+        return None
+
+sink = initialize_stix_sink(STIX_SINK_DIRECTORY)
 
 # Use a TAXIICollectionSource to interact with your TAXII server
-collection_source = TAXIICollectionSource(TAXII_COLLECTION_URL)
+TAXII_COLLECTION_URL = os.getenv("TAXII_COLLECTION_URL")
 
-try:
-    # Fetch data from the TAXII server and store it locally
-    sink.save_data(collection_source)
-    logging.info("TAXII data fetched and stored successfully.")
-except Exception as e:
-    logging.error("Failed to fetch and store TAXII data: %s", str(e)
+def initialize_taxii_collection_source(url):
+    try:
+        collection_source = TAXIICollectionSource(url)
+        return collection_source
+    except Exception as e:
+        logging.error("Failed to initialize TAXII collection source: %s", str(e))
+        return None
+
+collection_source = initialize_taxii_collection_source(TAXII_COLLECTION_URL)
 
 # Initialize a FileSystemSource to access the locally stored data
 STIX_SOURCE_DIRECTORY = "/path/to/stix"
-source = FileSystemSource(STIX_SOURCE_DIRECTORY)
+
+def initialize_stix_source(directory):
+    try:
+        source = FileSystemSource(directory)
+        return source
+    except Exception as e:
+        logging.error("Failed to initialize STIX source: %s", str(e))
+        return None
+
+source = initialize_stix_source(STIX_SOURCE_DIRECTORY)
 
 # Load network logs into a pandas DataFrame
-log_data = []
+NETWORK_LOG_FILE = os.getenv("NETWORK_LOG_FILE")
 
-try:
-    with open(NETWORK_LOG_FILE, 'r') as file:
-        for line in file:
-            parts = line.split()  # Split the log entry by whitespace
-            if len(parts) >= 3:
-                timestamp = parts[0]
-                log_level = parts[1]
-                message = ' '.join(parts[2:])
-                log_data.append({'Timestamp': timestamp, 'Log_Level': log_level, 'Message': message})
-    logging.info("Network logs loaded successfully.")
-except FileNotFoundError:
-    logging.error("Network log file not found.")
+def load_network_logs(log_file_path):
+    log_data = []
+    try:
+        with open(log_file_path, 'r') as file:
+            for line in file:
+                parts = line.split()  # Split the log entry by whitespace
+                if len(parts) >= 3:
+                    timestamp = parts[0]
+                    log_level = parts[1]
+                    message = ' '.join(parts[2:])
+                    log_data.append({'Timestamp': timestamp, 'Log_Level': log_level, 'Message': message})
+        logging.info("Network logs loaded successfully.")
+    except FileNotFoundError:
+        logging.error("Network log file not found.")
+    return pd.DataFrame(log_data)
 
-network_logs = pd.DataFrame(log_data)
+# Load network logs using the function
+network_logs = load_network_logs(NETWORK_LOG_FILE)
 
-# Set up your OpenAI API key
-openai.api_key = OPENAI_API_KEY
-openai.api_key = 'sk-xxxx'
+# Set up your OpenAI API key using environment variables
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+def set_openai_api_key(api_key):
+    try:
+        openai.api_key = api_key
+    except Exception as e:
+        logging.error("Failed to set OpenAI API key: %s", str(e))
+
+set_openai_api_key(OPENAI_API_KEY)
 
 # Function to create a STIX Indicator
 def create_stix_indicator():
@@ -74,14 +99,7 @@ def parse_stix_json(stix_json):
     return indicator
 
 # Function to detect suspicious network activity
-def detect_network_threats():
-    # Define a list of suspicious network activity patterns (you can expand this)
-    suspicious_patterns = [
-        'Unauthorized access attempt',
-        'Unusual data exfiltration',
-        # Add more patterns here
-    ]
-
+def detect_network_threats(network_logs, suspicious_patterns):
     threats_found = []
 
     for pattern in suspicious_patterns:
@@ -91,6 +109,8 @@ def detect_network_threats():
             threats_found.append(pattern)
 
     return threats_found
+
+suspicious_patterns = ['Unauthorized access attempt', 'Unusual data exfiltration']
 
 # Function to map detected threats to MITRE ATT&CK techniques
 def map_threats_to_attck(threats_found):
@@ -106,7 +126,7 @@ def map_threats_to_attck(threats_found):
     return list(mapped_techniques)
 
 # Function to find associated threat actors using ATT&CK data
-def find_threat_actors(technique_id):
+def find_threat_actors(source, technique_id):
     actors_found = set()
 
     # Query ATT&CK data to find threat actors using the specified technique
@@ -142,176 +162,58 @@ if __name__ == "__main__":
     stix_indicator = create_stix_indicator()
     print(stix_indicator)
 
-    import os
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import openai
-    from stix2 import FileSystemSource, FileSystemSink, TAXIICollectionSource, Filter, Indicator, parse
-    from taxii2client import Server, Collection
-    import git
+    # Parse a STIX JSON string into a Python STIX object
+    stix_json = """
+    {
+        "type": "indicator",
+        "spec_version": "2.1",
+        "id": "indicator--dbcbd659-c927-4f9a-994f-0a2632274394",
+        "created": "2017-09-26T23:33:39.829Z",
+        "modified": "2017-09-26T23:33:39.829Z",
+        "name": "File hash for malware variant",
+        "indicator_types": [
+            "malicious-activity"
+        ],
+        "pattern_type": "stix",
+        "pattern_version": "2.1",
+        "pattern": "[file:hashes.md5 ='d41d8cd98f00b204e9800998ecf8427e']",
+        "valid_from": "2017-09-26T23:33:39.829952Z"
+    }
+    """
+    parsed_indicator = parse_stix_json(stix_json)
+    print(parsed_indicator.serialize(pretty=True))
 
-    # Enhanced error handling with logging
-    import logging
-
-    logging.basicConfig(filename='app.log', level=logging.ERROR)
-
-
-    def handle_errors(func):
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                logging.error(str(e))
-                print("An error occurred. Check the logs for details.")
-
-        return wrapper
-
-
-    # Initialize a FileSystemSink to store the STIX/TAXII data locally
-    @handle_errors
-    def initialize_stix_sink(directory):
-        sink = FileSystemSink(directory)
-        return sink
-
-
-    stix_sink_directory = "/Users/admin/Desktop/log_data"
-    sink = initialize_stix_sink(stix_sink_directory)
-
-
-    # Use a TAXIICollectionSource to interact with your TAXII server
-    @handle_errors
-    def initialize_taxii_collection_source(url):
-        collection_source = TAXIICollectionSource(url)
-        return collection_source
+    threats_found = detect_network_threats(network_logs, suspicious_patterns)
+    if threats_found:
+        print("Detected threats:")
+        for threat in threats_found:
+            print(threat)
+        mapped_techniques = map_threats_to_attck(threats_found)
+        if mapped_techniques:
+            print("\nMapped MITRE ATT&CK techniques:")
+            for technique in mapped_techniques:
+                print(technique)
+                threat_actors = find_threat_actors(source, technique)
+                if threat_actors:
+                    print("Associated threat actors:")
+                    for actor in threat_actors:
+                        print(actor)
 
 
-    taxii_collection_url = "https://otx.alienvault.com/taxii/collections/9fe35b0b-5a9e-4c08-a6d0-251f0cf5318b"
-    collection_source = initialize_taxii_collection_source(taxii_collection_url)
+    # Fetch threat intelligence feeds
+    threat_feed_urls = [
+        'http://danger.rulez.sk/projects/bruteforceblocker/blist.php',
+        'https://dataplane.org/',
+        'http://rules.emergingthreats.net/blockrules/compromised-ips.txt',
+        # Add more feed URLs here
+    ]
+    threat_indicators = fetch_threat_intelligence_feeds(threat_feed_urls)
 
-    # Initialize a FileSystemSource to access the locally stored data
-    stix_source_directory = "/Users/admin/Desktop/stix"
-    source = FileSystemSource(stix_source_directory)
-
-
-    # Load network logs into a pandas DataFrame
-    @handle_errors
-    def read_network_logs(log_file):
-        log_data = []
-        with open(log_file, 'r') as file:
-            for line in file:
-                parts = line.split()  # Split the log entry by whitespace
-                if len(parts) >= 3:
-                    timestamp = parts[0]
-                    log_level = parts[1]
-                    message = ' '.join(parts[2:])
-                    log_data.append({'Timestamp': timestamp, 'Log_Level': log_level, 'Message': message})
-        return pd.DataFrame(log_data)
-
-
-    network_log_file = '/var/log/appfirewall.log'
-    network_logs = read_network_logs(network_log_file)
-
-    # Set up your OpenAI API key using environment variables
-    openai.api_key = os.getenv("OPENAI_API_KEY", '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')
-
-    # Improved secrets management using environment variables
-    taxii_api_key = os.getenv("TAXII_API_KEY", 'your_taxii_api_key')
-
-    # Enhanced dependency management
-    try:
-        import some_dependency
-    except ImportError:
-        logging.error("Dependency 'some_dependency' not found.")
-        print("Please install required dependencies.")
-
-
-    # Access control for file system access
-    @handle_errors
-    def fetch_and_store_taxii_data(source, sink):
-        sink.save_data(source)
-
-
-    fetch_and_store_taxii_data(collection_source, sink)
-
-
-    # Properly handle external service integration, including timeouts and retries
-    @handle_errors
-    def fetch_data_from_external_service(url):
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        return response.text
-
-
-    # Data validation and sanitization for URLs
-    @handle_errors
-    def validate_and_sanitize_url(url):
-        if not url.startswith('http://') and not url.startswith('https://'):
-            raise ValueError("Invalid URL format")
-        return url
-
-
-    threat_feed_url = os.getenv("THREAT_FEED_URL", 'https://example.com/threat-feed')
-    sanitized_url = validate_and_sanitize_url(threat_feed_url)
-
-    try:
-        threat_feed_data = fetch_data_from_external_service(sanitized_url)
-    except Exception as e:
-        logging.error(str(e))
-        print("Failed to fetch data from the threat feed. Check the logs for details.")
-
-
-    # File system access control
-    @handle_errors
-    def clone_repository(repository_url, clone_dir):
-        if not os.path.exists(clone_dir):
-            git.Repo.clone_from(repository_url, clone_dir)
-
-
+    # Clone MITRE ATT&CK scripts repository
     mitre_repository_url = 'https://github.com/mitre-attack/attack-scripts.git'
     mitre_clone_dir = 'mitre-attack-scripts'
-    clone_repository(mitre_repository_url, mitre_clone_dir)
+    clone_mitre_attack_scripts(mitre_repository_url, mitre_clone_dir)
 
-    # Example usage
 
-    if __name__ == "__main__":
-        # Create a STIX Indicator
-        stix_indicator = create_stix_indicator()
-        print(stix_indicator)
 
-        # Parse a STIX JSON string into a Python STIX object
-        stix_json = """
-        {
-            "type": "indicator",
-            "spec_version": "2.1",
-            "id": "indicator--dbcbd659-c927-4f9a-994f-0a2632274394",
-            "created": "2017-09-26T23:33:39.829Z",
-            "modified": "2017-09-26T23:33:39.829Z",
-            "name": "File hash for malware variant",
-            "indicator_types": [
-                "malicious-activity"
-            ],
-            "pattern_type": "stix",
-            "pattern_version": "2.1",
-            "pattern": "[file:hashes.md5 ='d41d8cd98f00b204e9800998ecf8427e']",
-            "valid_from": "2017-09-26T23:33:39.829952Z"
-        }
-        """
-        parsed_indicator = parse_stix_json(stix_json)
-        print(parsed_indicator.serialize(pretty=True))
 
-        # Continue with your existing code
-        threats_found = detect_network_threats(network_logs)
-        if threats_found:
-            print("Detected threats:")
-            for threat in threats_found:
-                print(threat)
-            mapped_techniques = map_threats_to_attck(threats_found)
-            if mapped_techniques:
-                print("\nMapped MITRE ATT&CK techniques:")
-                for technique in mapped_techniques:
-                    print(technique)
-                    threat_actors = find_threat_actors(source, technique)
-                    if threat_actors:
-                        print("Associated threat actors:")
-                        for actor in threat_actors:
-                            print(actor)
